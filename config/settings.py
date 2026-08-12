ce7c3b759eaaf356a2a4a56725d800b280919fed
tulshiG <tulshi123123@gmail.com>
2026-08-12 16:25:37 +0530

    Add STATIC_ROOT setting

--- config/settings.py
+++ config/settings.py
@@ -79,7 +79,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = 'static/'
-
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 # Media (uploaded work report images)
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
